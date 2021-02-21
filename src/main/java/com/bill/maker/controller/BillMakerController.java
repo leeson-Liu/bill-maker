62e8ab35c7a177a6b4ac355d096d9b40469078f0
leeson-Liu <305671879@qq.com>
2021-02-21 20:03:10 +0800

    init

--- src/main/java/com/bill/maker/controller/BillMakerController.java
+++ src/main/java/com/bill/maker/controller/BillMakerController.java
@@ -1,13 +1,19 @@
 package com.bill.maker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("BillMakerController")
 @RequestMapping("/bill")
 public class BillMakerController {
 
+    @GetMapping("/helloWorld")
+    public String hello(@RequestParam(value = "word") String word) {
 
+        return "ok " + word;
+    }
 
 
 }
