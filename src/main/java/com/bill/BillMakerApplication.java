2409e296e7c45abd8c2ef3e538b8a8c5c491916b
leeson-Liu <305671879@qq.com>
2021-02-21 18:55:06 +0800

    试试提交

---
+++ src/main/java/com/bill/BillMakerApplication.java
@@ -0,0 +1,12 @@
+package com.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillMakerApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BillMakerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/bill/maker/controller/BillMakerController.java
@@ -0,0 +1,13 @@
+package com.bill.maker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("BillMakerController")
+@RequestMapping("/bill")
+public class BillMakerController {
+
+
+
+
+}
