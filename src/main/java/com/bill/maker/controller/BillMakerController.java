4b13e8f263b71b447fe4d4b6ce14ffcc100abbdb
cuibo_yan@163.com <cby19950113>
2021-02-27 16:34:30 +0900

    pdf输出实现

--- src/main/java/com/bill/maker/controller/BillMakerController.java
+++ src/main/java/com/bill/maker/controller/BillMakerController.java
@@ -1,8 +1,14 @@
 package com.bill.maker.controller;
 
+import com.bill.maker.entity.Bill;
 import com.bill.maker.entity.Good;
 import com.bill.maker.service.BillService;
 import com.bill.maker.utils.ExcelUtils;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 //import com.bill.maker.utils.ReadExcelFilesUtils;
 
@@ -23,7 +31,9 @@ public class BillMakerController {
 
     @Autowired
     private BillService billService;
-    public static String filepath = "D:\\file\\";
+    public static String FILEPATH = "D:\\file\\";
+    // 账单最大商品种类
+    public static int MAX_GOODS_NUM = 15;
 
     @GetMapping("/helloWorld")
     public String hello(@RequestParam(value = "word") String word) {
@@ -38,9 +48,16 @@ public class BillMakerController {
     @GetMapping("/upload")
     public String upload(@RequestParam(value = "filename") String filename) {
         log.info("开始读取excel文件");
-        String fileFullName = filepath + filename;
+        String fileFullName = FILEPATH + filename;
         List<Good> goodsList = ExcelUtils.readExcel(fileFullName, Good.class);
         log.info("文件读取结束");
+        log.info("创建映射数据");
+        Bill bill = new Bill();
+        bill.setGoodList(goodsList);
+        Map<String, Object> dataMap = creatDataMap(bill);
+        log.info("填充PDF模板");
+        fillPdfTemplate(dataMap);
+        log.info("打印数据");
         return goodsList.toString();
     }
 
@@ -74,5 +91,104 @@ public class BillMakerController {
         return goodsList;
     }
 
+    /**
+     * 创建映射数据
+     *
+     * @param bill 账单信息
+     */
+    private Map<String, Object> creatDataMap(Bill bill) {
+        List<Good> goodsList = bill.getGoodList();
+        Map<String, String> dataMap = new HashMap();
+        int textIndex = 2;
+        for (int index = 0; index < goodsList.size(); index++) {
+            if (index == MAX_GOODS_NUM) {
+                break;
+            }
+            // 品名
+            dataMap.put("fill_" + textIndex, goodsList.get(index).getName());
+            textIndex++;
+            // 单价
+            dataMap.put("fill_" + textIndex, goodsList.get(index).getRealPrice().toString());
+            textIndex++;
+            // 数量
+            dataMap.put("fill_"+textIndex,goodsList.get(index).getNum().toString());
+            textIndex++;
+            // 金额
+            dataMap.put("fill_" + textIndex, goodsList.get(index).getTotalPrice().toString());
+            textIndex++;
+        }
+        // 客户名称
+        dataMap.put("fill_1", bill.getGustName());
+        // 邮编号码
+        dataMap.put("yubin", bill.getPostalCode());
+        // 邮编号码
+        dataMap.put("address", bill.getAddress());
+        // 邮编号码
+        dataMap.put("daihyoName", bill.getRepresentName());
+        // 电话号码
+        dataMap.put("tel", bill.getTelNo());
+        // 时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        dataMap.put("time", sdf.format(new Date()));
+        // 请求号码
+        dataMap.put("requestNo", bill.getRequestNO());
+        Map<String, Object> mappingMap = new HashMap();
+        mappingMap.put("datemap",dataMap);
+        return mappingMap;
+    }
+
+    /**
+     * 填充PDF模板
+     *
+     * @param map 数据列表
+     */
+    private void fillPdfTemplate(Map<String, Object> map) {
+        // 模板路径
+        String templatePath = "D:\\file\\pdf\\請求書_表单.pdf";//原PDF模板
+        // 生成的新文件路径
+        String newPDFPath = "D:\\file\\pdf\\請求書.pdf";
+        PdfReader reader;
+        FileOutputStream out;
+        ByteArrayOutputStream bos;
+        PdfStamper stamper;
+        try {
+            // 字体设置
+            BaseFont bf = BaseFont.createFont("C:/windows/fonts/simsun.ttc,1", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font FontChinese = new Font(bf, 12f, Font.BOLD);
+            // 输出流
+            out = new FileOutputStream(newPDFPath);
+            // 读取pdf模板
+            reader = new PdfReader(templatePath);
+            bos = new ByteArrayOutputStream();
+            stamper = new PdfStamper(reader, bos);
+            AcroFields form = stamper.getAcroFields();
+            Map<String, String> datemap = (Map<String, String>) map.get("datemap");
+            form.addSubstitutionFont(bf);
+            for (String key : datemap.keySet()) {
+                String value = datemap.get(key);
+                System.out.println(value + "*(*(*(**---：" + key);
+                form.setField(key, value);
+            }
+            stamper.setFormFlattening(true);// 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
+            stamper.close();
+            Document doc = new Document(PageSize.A4, 0, 0, 0, 0);
+            Font font = new Font(bf, 32);
+            PdfCopy copy = new PdfCopy(doc, out);
+            doc.open();
+            PdfImportedPage importPage = null;
+            ///循环是处理成品只显示一页的问题
+            for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+                importPage = copy.getImportedPage(new PdfReader(bos.toByteArray()), i);
+                copy.addPage(importPage);
+            }
+            doc.close();
+            System.err.println("生成pdf文件完成！");
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (DocumentException e) {
+            System.out.println(e);
+
+        }
+    }
 
 }
